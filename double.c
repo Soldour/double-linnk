cb9c9ac9094bd4dcc27a8bfc823619ebb323cced
Soldour <56756863+Soldour@users.noreply.github.com>
2020-03-17 23:36:17 +0200

    Update double.c

--- double.c
+++ double.c
@@ -11,15 +11,19 @@ struct node
 
 struct node createLink(int n);
 void printList();
-
+struct node* insertElement(int value, int bound);
+struct node* begning(int value);
 int main()
 {
 
 
-
-    createLink(6);
+    int n = 5;
+    createLink(n);
     printList();
-        
+  //  insertElement(308, n);
+   head= begning(33 );
+    printList();
+    
 
 
 
