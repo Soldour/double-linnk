e97622fcfb6981774aa772143ffd97aded828ee0
Soldour <56756863+Soldour@users.noreply.github.com>
2020-04-30 09:00:55 -0700

    Update double.c

--- double.c
+++ double.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
-    int data;
-    struct node* left;
-    struct node* next;
 
-}*head, * current;
 
 struct node createLink(int n);
 void printList();
-
+struct node* insertElement(int value, int bound);
+struct node* begning(int value);
+void deletAllElement(int  key);
 int main()
 {
 
 
-
-    createLink(6);
+    int n = 5;
+    createLink(n);
     printList();
-        
+   insertElement(308, n);
+  
+  
+    printList();
 
 
 
